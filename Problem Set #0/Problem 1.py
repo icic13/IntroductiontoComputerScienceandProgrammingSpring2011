f6e6d544476f415175407dbac64e59de25dc3a0e
Md. Rana Mahmud <ranamd0@gmail.com>
2015-11-13 22:51:35 +0600

    Problem 1 completed

---
+++ Problem Set #0/Problem 1.py
@@ -0,0 +1,5 @@
+print ("What's your birthday ?")
+date = raw_input()
+print ("What's your last name ? ")
+lastName = raw_input()
+print ('Your last name is '+lastName + ' and birthday is '+date+" .")
